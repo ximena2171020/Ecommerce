36582e9310778af32212c356fb5f35dbd88463f6
Mena <ximenaab06@gmail.com>
2025-05-07 23:44:33 -0500

    NewStructureAndCountryEntity

---
+++ Ecommerce.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options) : base(options) { 
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x=>x.Name).IsUnique();
+        }
+    }
+}
---
+++ Ecommerce.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Ecommerce.Shared.Entities
+{
+    public class Country
+    {
+        public int id { get; set; }
+
+        [Display(Name ="País")]
+        [Required(ErrorMessage = " El campo{0} Es Obligatorio ")]
+        [MaxLength(100)]
+
+        public string Name { get; set; }
+
+
+    }
+}
