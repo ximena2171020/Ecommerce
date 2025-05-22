e1149b9bdcc8cc64f8030ea73331821c47443715
Mena <ximenaab06@gmail.com>
2025-05-21 19:05:17 -0500

    Eliminando archivos innecesarios del seguimiento de Git

--- Ecommerce.API/Data/DataContext.cs
+++ Ecommerce.API/Data/DataContext.cs
@@ -3,18 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.API.Data
 {
-    public class DataContext :DbContext
+    public class DataContext : DbContext
     {
-        public DataContext (DbContextOptions<DataContext> options) : base(options) { 
-        }
-
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Country> Countries { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<State> States { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Country>().HasIndex(x=>x.Name).IsUnique();
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+            modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique();
+            modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name);
+            modelBuilder.Entity<Category>().HasIndex(x => x.Name).IsUnique();
+            modelBuilder.Entity<ProductCategory>().HasIndex(pc => new { pc.ProductId, pc.CategoryId }).IsUnique();
+
         }
     }
 }
